d21bea5c4df205c1c8eb8e618053dbeca3ffbdff
Zee-Yeon <zee.zee.yeon.00@gmail.com>
2024-03-21 22:02:27 +0900

    build : security 설정

--- backend/src/main/java/com/write/reco/config/SecurityConfig.java
+++ backend/src/main/java/com/write/reco/config/SecurityConfig.java
@@ -1,4 +1,67 @@
 package com.write.reco.config;
 
+import com.write.reco.security.AuthorizationFilter;
+import com.write.reco.security.JwtProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtProvider jwtProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable);
+
+        http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()));
+
+        http
+                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers("/user/mypage").authenticated()
+                        .anyRequest().permitAll());
+
+        http
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http
+                .addFilterBefore(new AuthorizationFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin("http://10.125.121.203:3000");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        config.addExposedHeader("Authorization");
+        config.addExposedHeader("email");
+        config.addExposedHeader("Location");
+        config.setAllowCredentials(true); // 요청/응답에 자격증명정보 포함을 허용
+
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
 }
