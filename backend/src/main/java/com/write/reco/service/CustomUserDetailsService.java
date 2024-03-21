ce45deab6a458a2ecb8fb5a548607e9e48060e0a
Zee-Yeon <zee.zee.yeon.00@gmail.com>
2024-03-21 22:07:01 +0900

    feat : userDetailService, email로 설정

---
+++ backend/src/main/java/com/write/reco/service/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.write.reco.service;
+
+import com.write.reco.advice.exception.CustomException;
+import com.write.reco.advice.response.ResponseCode;
+import com.write.reco.domain.User;
+import com.write.reco.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .map(this::createUserDetails)
+                .orElseThrow(() -> new CustomException(ResponseCode.NOT_FOUND_USER));
+
+    }
+
+    private UserDetails createUserDetails(User user) {
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .build();
+    }
+}
