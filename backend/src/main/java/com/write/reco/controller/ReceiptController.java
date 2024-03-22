cf8eee48f66217e356686627b55f21dcd58e2775
Zee-Yeon <zee.zee.yeon.00@gmail.com>
2024-03-22 21:43:44 +0900

    chore : test code 주석 처리

--- backend/src/main/java/com/write/reco/controller/ReceiptController.java
+++ backend/src/main/java/com/write/reco/controller/ReceiptController.java
@@ -7,10 +7,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.MalformedURLException;
 
 import static com.write.reco.advice.response.ResponseCode.SUCCESS_SAVE_RECEIPT;
 
@@ -22,9 +21,29 @@ public class ReceiptController {
 
     private final ReceiptService receiptService;
 
-    @PostMapping
-    public ResponseEntity<?> saveReceipt(@AuthenticationPrincipal User auth, @RequestBody ReceiptRequest receiptRequest) {
-        receiptService.saveReceipt(auth, receiptRequest);
+//    @PostMapping("/save")
+//    public ResponseEntity<?> saveReceipt(@AuthenticationPrincipal User auth, @RequestBody ReceiptRequest receiptRequest) {
+//        receiptService.saveReceipt(auth, receiptRequest);
+//        return new ResponseEntity<>(Response.create(SUCCESS_SAVE_RECEIPT, null), SUCCESS_SAVE_RECEIPT.getHttpStatus());
+//    }
+
+    @PostMapping("/save")
+    public ResponseEntity<?> saveReceipt(@AuthenticationPrincipal User auth, @RequestBody ReceiptRequest request) {
+        receiptService.saveReceipt(auth, request);
         return new ResponseEntity<>(Response.create(SUCCESS_SAVE_RECEIPT, null), SUCCESS_SAVE_RECEIPT.getHttpStatus());
     }
+
+    // 업체명, 물품명, 거래기간
+//    @GetMapping
+//    public ResponseEntity<?> searchReceipt(@AuthenticationPrincipal User auth,
+//                                           @RequestParam(required = false) String company,
+//                                           @RequestParam(required = false) String item,
+//
+//                                            )
+
+//    @PostMapping("/test")
+//    public ResponseEntity<?> test(@RequestParam("path") String path) throws MalformedURLException {
+//        String filename = receiptService.filename(path);
+//        return new ResponseEntity<>(Response.create(SUCCESS_SAVE_RECEIPT, filename), SUCCESS_SAVE_RECEIPT.getHttpStatus());
+//    }
 }
