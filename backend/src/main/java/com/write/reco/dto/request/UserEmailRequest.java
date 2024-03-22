ab1804486a61dcae4a37296411a0f8fa2e016a60
Zee-Yeon <zee.zee.yeon.00@gmail.com>
2024-03-22 21:47:34 +0900

    chore : unique, email validation 추가

--- backend/src/main/java/com/write/reco/dto/request/UserEmailRequest.java
+++ backend/src/main/java/com/write/reco/dto/request/UserEmailRequest.java
@@ -1,11 +1,15 @@
 package com.write.reco.dto.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import org.hibernate.validator.constraints.UniqueElements;
 
 @Getter
 public class UserEmailRequest {
 
     @NotNull
+    @UniqueElements
+    @Email(message = "이메일 형식이 아닙니다.")
     private String email;
 }
