d348a1966c92aaccfc1f01874b2f045e8e6688a9
Zee-Yeon <zee.zee.yeon.00@gmail.com>
2024-03-22 21:38:38 +0900

    chore : log 확인

--- backend/src/main/java/com/write/reco/controller/ImageController.java
+++ backend/src/main/java/com/write/reco/controller/ImageController.java
@@ -6,6 +6,7 @@ import com.write.reco.advice.response.Response;
 import com.write.reco.service.ImageService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
@@ -23,13 +24,15 @@ import static com.write.reco.advice.response.ResponseCode.SUCCESS_RECEIPT_OCR;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/image")
+@Slf4j
 public class ImageController {
 
     private final ImageService imageService;
 
     @PostMapping("/upload")
-    public ResponseEntity<?> uploadImage(@AuthenticationPrincipal User user, @Valid @RequestParam("file")MultipartFile file) throws IOException {
-        ResponseEntity<JsonNode> entity = imageService.uploadImage(user, file);
+    public ResponseEntity<?> uploadImage(@AuthenticationPrincipal User auth, @Valid @RequestParam("file")MultipartFile file) throws IOException {
+        ResponseEntity<JsonNode> entity = imageService.uploadImage(auth, file);
+        log.info("entity={}", entity.getBody());
         return new ResponseEntity<>(Response.create(SUCCESS_RECEIPT_OCR, entity), SUCCESS_RECEIPT_OCR.getHttpStatus());
     }
 }
