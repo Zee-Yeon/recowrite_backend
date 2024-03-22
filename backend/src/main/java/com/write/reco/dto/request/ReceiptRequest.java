7665933c90975e91d0242bd83026c768e1fbb946
Zee-Yeon <zee.zee.yeon.00@gmail.com>
2024-03-22 21:44:59 +0900

    chore : inner class, json 참조 문제 static 변경

--- backend/src/main/java/com/write/reco/dto/request/ReceiptRequest.java
+++ backend/src/main/java/com/write/reco/dto/request/ReceiptRequest.java
@@ -1,19 +1,31 @@
 package com.write.reco.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ReceiptRequest {
+
     private String image;
+
     private String company;
+
     private String tradeAt;
+
     private int sum;
+
     private List<Items> itemList;
 
     @Getter
-    public class Items {
+    public static class Items {
         private String item;
         private int unitPrice;
         private int quantity;
