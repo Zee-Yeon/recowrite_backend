63581d125862c7aa99abf8aed9855bd18d02e2d1
Zee-Yeon <zee.zee.yeon.00@gmail.com>
2024-03-23 21:53:51 +0900

    feat : 회사명으로 조회하기 (페이징)

--- backend/src/main/java/com/write/reco/repository/ReceiptRepository.java
+++ backend/src/main/java/com/write/reco/repository/ReceiptRepository.java
@@ -1,9 +1,15 @@
 package com.write.reco.repository;
 
+import com.write.reco.domain.Item;
 import com.write.reco.domain.Receipt;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ReceiptRepository extends JpaRepository<Receipt, Long> {
+
+    @Query("select r from Receipt r where r.image.user.email = :email and r.company = :company")
+    Page<Receipt> findReceiptByImageByAndCompany(String email, String company, Pageable pageable);
+
 }
