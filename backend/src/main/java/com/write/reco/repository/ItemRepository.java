f5cb94e0e2c911afe14d65af5e4ca8ed5586c70c
Zee-Yeon <zee.zee.yeon.00@gmail.com>
2024-03-23 21:49:16 +0900

    remove : 주석 삭제

--- backend/src/main/java/com/write/reco/repository/ItemRepository.java
+++ backend/src/main/java/com/write/reco/repository/ItemRepository.java
@@ -1,6 +1,8 @@
 package com.write.reco.repository;
 
 import com.write.reco.domain.Item;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
