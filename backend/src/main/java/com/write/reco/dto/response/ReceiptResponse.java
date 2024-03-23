8431470e0c1089f5755524fc56b949b9497e1328
Zee-Yeon <zee.zee.yeon.00@gmail.com>
2024-03-23 21:54:10 +0900

    feat : ReceiptResponse dto

---
+++ backend/src/main/java/com/write/reco/dto/response/ReceiptResponse.java
@@ -0,0 +1,34 @@
+package com.write.reco.dto.response;
+
+import com.write.reco.domain.Receipt;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReceiptResponse {
+    private String company;
+    private String tradAt;
+    private int sum;
+    private List<ItemResponse> itemList;
+
+
+    public static ReceiptResponse dto(Receipt receipt) {
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        return ReceiptResponse.builder()
+                .company(receipt.getCompany())
+                .tradAt(receipt.getTradeAt().format(format))
+                .sum(receipt.getSum())
+                .itemList(receipt.getItemList().stream().map(ItemResponse::dto).collect(Collectors.toList()))
+                .build();
+    }
+}
