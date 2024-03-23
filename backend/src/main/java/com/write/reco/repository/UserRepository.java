4decfccd8681afc7f404d1c674c34cea3017045c
Zee-Yeon <zee.zee.yeon.00@gmail.com>
2024-03-23 18:32:41 +0900

    feat : 활동 중인 사용자만 찾을 수 있도록 쿼리문 추가

--- backend/src/main/java/com/write/reco/repository/UserRepository.java
+++ backend/src/main/java/com/write/reco/repository/UserRepository.java
@@ -2,9 +2,13 @@ package com.write.reco.repository;
 
 import com.write.reco.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
+
+    @Query("select u from User u where u.email = :email and u.status = 'ACTIVE'")
+    Optional<User> findByEmail(@Param("email") String email);
 }
