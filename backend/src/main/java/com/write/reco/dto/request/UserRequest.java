fa1db5bf0a4c6dd09e964b7c0ce65273cc1e0d03
Zee-Yeon <zee.zee.yeon.00@gmail.com>
2024-03-25 10:04:59 +0900

    refactor : 컬럼 unique 삭제, 탈퇴 후 같은 아이디로 재가입 가능한 경우 고려함

--- backend/src/main/java/com/write/reco/dto/request/UserRequest.java
+++ backend/src/main/java/com/write/reco/dto/request/UserRequest.java
@@ -3,14 +3,12 @@ package com.write.reco.dto.request;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.hibernate.validator.constraints.UniqueElements;
 
 @AllArgsConstructor
 @Getter
 public class UserRequest {
 
     @NotNull
-    @UniqueElements
     private String email;
 
     @NotNull
