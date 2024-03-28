b02e2888451146bd180db3bc89e7459da477ef45
Zee-Yeon <zee.zee.yeon.00@gmail.com>
2024-03-28 16:37:52 +0900

    feat : 가격 validation(최소, 최대값)

--- backend/src/main/java/com/write/reco/dto/request/ReceiptRequest.java
+++ backend/src/main/java/com/write/reco/dto/request/ReceiptRequest.java
@@ -1,7 +1,9 @@
 package com.write.reco.dto.request;
 
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
+import org.hibernate.validator.constraints.Range;
 
 import java.util.List;
 
@@ -17,16 +19,27 @@ public class ReceiptRequest {
 
     private String tradeAt;
 
+    @Range(min = 0, max = 1000000000)
     private int sum;
 
     private List<Items> itemList;
 
     @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
     public static class Items {
         private Long itemId;
+
         private String item;
+
+        @Range(min = 0, max = 10000000)
         private int unitPrice;
+
+        @Max(999)
         private int quantity;
+
+        @Range(min = 0, max = 100000000)
         private int price;
     }
 }
--- backend/src/main/java/com/write/reco/dto/request/ReceiptUpdate.java
+++ backend/src/main/java/com/write/reco/dto/request/ReceiptUpdate.java
@@ -1,6 +1,8 @@
 package com.write.reco.dto.request;
 
+import jakarta.validation.constraints.Max;
 import lombok.*;
+import org.hibernate.validator.constraints.Range;
 
 import java.util.List;
 
@@ -10,15 +12,26 @@ import java.util.List;
 @NoArgsConstructor
 public class ReceiptUpdate {
 
+    @Range(min = 0, max = 1000000000)
     private int sum;
 
     private List<Items> itemList;
 
     @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
     public static class Items {
+
         private String item;
+
+        @Range(min = 0, max = 10000000)
         private int unitPrice;
+
+        @Max(999)
         private int quantity;
+
+        @Range(min = 0, max = 100000000)
         private int price;
     }
 }
